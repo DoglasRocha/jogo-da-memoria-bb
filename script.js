717780123171a216f2563c3b64bbda1bb9cd1d31
DoglasRocha <doglas_rocha@protonmail.ch>
2021-09-11 00:58:29 -0300

    remove temporary files

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+	if (lockBoard) return;
+
 	this.classList.add('flip'); // toggle tira e adiciona classe, add só adiciona //
+
 	if (!hasFlippedCard) {
 		hasFlippedCard = true;
 		firstCard = this;
@@ -22,6 +26,18 @@ function checkForMatch() {
 	}
 
 	unflipCards();
+}
+
+function disableCards() {
+	firstCard.removeEventListener('click', flipCard);
+	secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+	setTimeout(() => {
+		firstCard.classList.remove('flip');
+		secondCard.classList.remove('flip');
+	}, 1500);
 }
 
 cards.forEach((card) => {
